656538f0f4110934433de1fdccc339d9562f4c42
Gabriel Nascimento <gabriel.nascimento@grupointegrado.br>
2024-06-13 13:26:28 -0300

    Estilização login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,48 +1,49 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./Login.module.css"
-import {z} from "zod"
-import {useState} from "react";
-import {useForm} from "react-hook-form";
-import {zodResolver} from "@hookform/resolvers/zod";
+import { z } from "zod"
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const loginSchema = z.object({
-    email: z.string().email({message: "E-mail inválido!"}),
-    password: z.string().min(6, {message: "A senha deve conter no mínimo 6 caractéres!"}),
+    email: z.string().email({ message: "E-mail inválido!" }),
+    password: z.string().min(6, { message: "A senha deve conter no mínimo 6 caractéres!" }),
 })
 
 const Login = () => {
 
-    const {register, handleSubmit, formState: {errors}} = useForm({
+    const { register, handleSubmit, formState: { errors } } = useForm({
         resolver: zodResolver(loginSchema)
     })
 
     const navigate = useNavigate()
 
     const handleForm = (data) => {
-        if(data) navigate('/list')
+        if (data) navigate('/list')
     }
 
     return (
         <div className={styles.container}>
             <form onSubmit={handleSubmit(handleForm)} className={styles.card}>
                 <div className={styles.card_header}>
-                    <h1>Login</h1>
+                    <h1>login</h1>
                 </div>
                 <div className={styles.card_body}>
                     <div>
-                        <label htmlFor="email">E-mail: </label>
-                        <input type="text" {...register('email')} id="email" placeholder="Digite seu e-mail." />
+                        <label htmlFor="email">e-mail: </label>
+                        <input type="text" {...register('email')} id="email" placeholder="write your email correctly." />
                         {errors.email && <p className={styles.error}>{errors.email.message}</p>}
                     </div>
                     <div>
-                        <label htmlFor="password">Senha: </label>
-                        <input type="password" {...register('password')} id="password" placeholder="Digite sua senha." />
+                        <label htmlFor="password">password: </label>
+                        <input type="password" {...register('password')} id="password" placeholder="try to remember your password." />
                         {errors.password && <p className={styles.error}>{errors.password.message}</p>}
                     </div>
+                    <div className={styles.card_footer}>
+                        <button type="submit" className={styles.btn} ><img src="imgs/confirm.svg" alt="" /></button>
+                    </div>
                 </div>
-                <div className={styles.card_footer}>
-                    <button type="submit" className={styles.btn} >Confirmar</button>
-                </div>
+
             </form>
 
         </div>
